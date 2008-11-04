39866d5b63df978c856eb776db6fe0f500aa65c8
James Golick <james@giraffesoft.ca>
2008-11-03 19:59:17 -0500

    add symlink file borrowed from jonathan

---
+++ symlink_it.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# 
+# "borrowed" from Jonathan Palardy (http://github.com/jpalardy/etc_config/tree/master)
+ 
+function relink() {
+  rm -i $1
+  ln -sn $2 $1
+}
+ 
+cd ..
+ 
+relink .bash_profile etc/bash_profile
+ 
+relink .vim          etc/vim
+relink .vimrc        etc/vimrc
+ 
+relink .gitconfig    etc/gitconfig
+relink .gitignore    etc/gitignore
+
