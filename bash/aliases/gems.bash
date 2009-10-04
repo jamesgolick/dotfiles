d13bf4a7c5a62dd3c957c0874b72fd2928bb64d7
James Golick <james@giraffesoft.ca>
2009-10-04 10:36:20 -0700

    add function for opening a gem's root dir in EDITOR

--- bash/aliases/gems.bash
+++ bash/aliases/gems.bash
@@ -3,3 +3,8 @@
 function cdgem() {
   cd $GEMDIR/gems/`ls /Library/Ruby/Gems/1.8/gems/ | grep $1-[0-9] | sort -r |head -n1`
 }
+
+function editgem() { 
+  $EDITOR $GEMDIR/gems/`ls /Library/Ruby/Gems/1.8/gems/ | grep $1-[0-9] | sort -r |head -n1`
+}
+
