6f35e3e4298d34168f3887793454a4df02b9ff59
James Golick <jamesgolick@gmail.com>
2010-09-29 22:16:02 -0700

    add script to force symlink

---
+++ symlink_it_force.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# 
+# "borrowed" from Jonathan Palardy (http://github.com/jpalardy/etc_config/tree/master)
+ 
+function relink() {
+  rm $1
+  ln -sn $2 $1
+}
+ 
+cd ..
+ 
+relink .bash_profile etc/bash_profile
+relink .bash	     etc/bash
+ 
+relink .vim          etc/vim
+relink .vimrc        etc/vimrc
+ 
+relink .gitconfig    etc/gitconfig
+relink .gitignore    etc/gitignore
+relink .screenrc     etc/screenrc
+
